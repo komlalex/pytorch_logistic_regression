6c388f94dda5d2e28470c24a69adcaf13ed8385a
Alexander Komla <kunalexander360@gmail.com>
2025-03-18 09:37:27 +0000

    Build custom model by extending nn.Module class

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,11 +1,14 @@
 """Working with Images and Logistic Regression""" 
 import torch 
 import torch.nn as nn 
-from torch.utils.data import random_split
+from torch.utils.data import random_split 
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
 import torchvision 
 from torchvision.datasets import MNIST 
 import torchvision.transforms as transforms
+
 import matplotlib.pyplot as plt  
 
 # Download training dataset 
@@ -82,4 +85,102 @@ Since there's no predefined validation set, we must manually split the 60,000 im
 datasets. Let's set aside 10,000 randomly chosen images for validation. We can do this using the random_split method from PyTorch
 """
 train_ds, val_ds = random_split(dataset, [50_000, 10_000]) 
-print(len(train_ds), len(val_ds))
+#print(len(train_ds), len(val_ds)) 
+
+"""It's essential to choose a random sample for creating a validation set. Training data is 
+often sorted by the target labels. 
+
+We can now create data loaders to help us load the data into batches. We'll use a batch size of 128"""
+BATCH_SIZE = 128 
+train_dl = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+val_dl = DataLoader(val_ds, batch_size=BATCH_SIZE) 
+
+"""We set shuffle=True for the training data loader to ensure that the batches generated in each epoch are different. This 
+randomization helps generalize & speed up the training process. On the other hand, since the valiadation data loader
+is used only for evaluating the model, there is no need to shuffle the images."""
+
+"""MODEL
+Now that we have prepared our data loaders, we can define our model. 
+*  A logistic regression model is almost identical to a linear regresion model. It contains 
+weights and bias matrices, and the output is obtained using simple matrix operations
+(pred = x @ w.t() + b). 
+* As we did with linear regression, we can use nn.Linear to create the model instead of manually
+creating and initalizing the matrices. 
+* Since nn.Linear expects each training example to be vectors, each 1x28x28 image tensor is flattened into a vector of size 784 (28x28) before being pased into the model.
+* The output for each image is a vector of size 10, with each element signifying the probability of a particular label (0 to 9). The predicted label for an image is simply the one 
+with the highest probability.""" 
+input_size = 28*28 
+num_classes = 10  
+
+# Logistic regression model 
+model = nn.Linear(input_size, num_classes) 
+
+"""Of course, our model is a lot larger than  our previous model in terms of the number of parameters. Let's take a look at 
+the weights and biases"""
+#print(model.weight.shape)
+#print(model.bias.shape) 
+
+"""Although there are a total of 7850 parameters here, conceptually, nothing has changed so far. Let's try and generate some outputs using our model. We'll 
+take the first 100 images from our dataset and pass them into our model"""
+
+#for images, labels in train_dl:
+#   print(labels)
+#   print(images.shape)
+#   outputs = model(images)  
+#   break
+
+
+"""The code above leads to an eror because our input data does not have the right shape. Our images are of the dhape 1x28x28, but we 
+need them to be of vectors of size 784, i.e., we need to flatten them. We'll use the .reshape method of a tensor, which 
+will allow us to efficently view each image as a flat vector without really creating a copy of the underlying data. To include this additional funcionality within our model, we need to define a custom model by extending nn.Module class from PyTorch.
+
+A class in Python provides a "blueprint" for creating objects. Let's look at an example of defining a new class in Python."""
+
+class Person: 
+    # class constructor 
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age  
+    # Method 
+    def say_hello(self): 
+        print(f"Hello my name is {self.name}!") 
+
+"""Here's how we create or instantiate an object of the class Person""" 
+alex = Person("Alex", 25) 
+#alex.say_hello()
+
+"""Classes can also be build upon or textend the functionality of existing clases. Let's extend
+the nn.Module class from PyTorch to define a custom model""" 
+
+class MnistModel(nn.Module): 
+    def __init__(self,):
+        super().__init__() 
+        self.linear = nn.Linear(input_size, num_classes) 
+
+    def forward(self, xb) -> torch.Tensor: 
+        xb = xb.reshape(-1, 784) 
+        out = self.linear(xb) 
+        return out  
+
+model = MnistModel() 
+
+for xb, yb in train_dl: 
+    print(xb.shape)
+    outputs = model(xb) 
+    print(f"Outputs shape: {outputs.shape}") 
+    print(f"Sample outputs: \n{outputs[:2].data}") 
+
+    # Apply softmax for each output row 
+    probs = F.softmax(outputs, dim=1) 
+
+    # Look at sample probabilities 
+    print(f"Sample probabilities: \n{probs[:2]}")
+
+    # Add up the probabilties of an output row 
+    print(f"Sum: {torch.sum(probs[0]).item()}")
+
+    break  
+
+"""To convert the output rows into proberbilities, we use the softmax function. 
+While it's easy to implement the softmax function, we'll use the implementation that's provided withing PyTorch because it works well with 
+multidimensional tensors (a list of output rows in our case)""" 
